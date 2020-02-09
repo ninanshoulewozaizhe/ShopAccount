a71a750a1c1e35a5be854412c9c1093f01777042
ninanshoulewozaizhe <172685516@qq.com>
2020-02-09 20:07:34 +0800

    修改tslint

--- SA-fe/shopaccount-fe/src/router/index.ts
+++ SA-fe/shopaccount-fe/src/router/index.ts
@@ -9,7 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'Login',
-    component: Login,
+    component: Login
   },
   {
     path: '/about',
@@ -17,14 +17,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
