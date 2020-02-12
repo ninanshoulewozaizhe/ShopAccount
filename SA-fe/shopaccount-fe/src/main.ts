a557cfcdf659ab5aab00184979cab81f444079b8
ninanshoulewozaizhe <172685516@qq.com>
2020-02-12 16:06:34 +0800

    销售情况页面初始化

--- SA-fe/shopaccount-fe/src/main.ts
+++ SA-fe/shopaccount-fe/src/main.ts
@@ -3,9 +3,15 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ElementUI from 'element-ui';
+import VeLine from 'v-charts/lib/line.common';
+import VeHistogram from 'v-charts/lib/histogram.common';
+import VePie from 'v-charts/lib/pie.common';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
+Vue.component(VeLine.name, VeLine);
+Vue.component(VeHistogram.name, VeHistogram);
+Vue.component(VePie.name, VePie);
 Vue.config.productionTip = false;
 
 new Vue({
--- SA-fe/shopaccount-fe/src/views/salesStatus/SalesStatus.vue
+++ SA-fe/shopaccount-fe/src/views/salesStatus/SalesStatus.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
-      salesStatus
+      <div class="chart_container">
+        <div class="today_sales_statistics">
+          <h3>今日销售情况</h3>
+          <ve-histogram :data="chartData"></ve-histogram>
+        </div>
+        <div class="all_sales_statistics">
+          <h3>总销售情况</h3>
+          <ve-histogram :data="chartData"></ve-histogram>
+        </div>
+      </div>
+      <div class="sales_detail_table">
+        <h3>商品销量排行</h3>
+        <el-table :data="tableData">
+          <el-table-column prop="product" label="商品" width="250"></el-table-column>
+          <el-table-column prop="salesVolume" label="销量" width="180"></el-table-column>
+          <el-table-column prop="shop" label="店铺" width="300"></el-table-column>
+        </el-table>
+      </div>
     </div>
 </template>
 
@@ -11,10 +28,45 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
   name: 'salesStatus'
 })
 export default class SalesStatus extends Vue {
-
+  chartData = {
+    columns: ['店铺', '销量'],
+    rows: [
+      { 店铺: '1/1', 销量: 1393},
+      { 店铺: '1/2', 销量: 3530},
+      { 店铺: '1/3', 销量: 2923},
+      { 店铺: '1/4', 销量: 1723},
+      { 店铺: '1/5', 销量: 3792},
+      { 店铺: '1/6', 销量: 4593}
+    ]
+  };
+  tableData = [{
+    product: '韩版卫衣',
+    salesVolume: '11',
+    shop: '上海市普陀区金沙江路 1518 弄'
+  }, {
+    product: '韩版卫衣',
+    salesVolume: '11',
+    shop: '上海市普陀区金沙江路 1517 弄'
+  }, {
+    product: '韩版卫衣',
+    salesVolume: '11',
+    shop: '上海市普陀区金沙江路 1519 弄'
+  }, {
+    product: '韩版卫衣',
+    salesVolume: '11',
+    shop: '上海市普陀区金沙江路 1516 弄'
+  }];
 }
 </script>
 
 <style lang="scss" scoped>
-
+.chart_container {
+  display: flex;
+  .today_sales_statistics, .all_sales_statistics {
+    width: 40%;
+  }
+  .today_sales_statistics {
+    margin-right: 100px;
+  }
+}
 </style>
