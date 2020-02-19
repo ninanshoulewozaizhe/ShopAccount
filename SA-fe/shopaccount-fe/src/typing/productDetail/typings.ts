c410a49d5869bc9a9dd69e95cfa9d7b1c904efee
ninanshoulewozaizhe <172685516@qq.com>
2020-02-19 22:16:27 +0800

    添加typing文件夹

---
+++ SA-fe/shopaccount-fe/src/typing/productDetail/typings.ts
@@ -0,0 +1,26 @@
+export interface IkindToStocks {
+    [index: string]: number;
+}
+
+export interface ISuggestObj {
+    value: string;
+}
+
+export interface IndexOJ {
+    [index: string]: number;
+}
+
+export interface IProductDetailItem {
+    id: number;
+    name: string;
+    description: string;
+    img: any;
+    salesVolumes: number;
+    type: IkindToStocks;
+}
+
+export interface IProductSalesItem {
+    type: string;
+    amount: number;
+    disable: boolean;
+}
