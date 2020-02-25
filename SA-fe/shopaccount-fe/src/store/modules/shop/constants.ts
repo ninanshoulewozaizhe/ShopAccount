7e985cb17beb0cc04ae1d1ba7cd72c3651490f42
ninanshoulewozaizhe <172685516@qq.com>
2020-02-25 15:11:51 +0800

    商店store初始化

--- SA-fe/shopaccount-fe/src/store/modules/shop/constants.ts
+++ SA-fe/shopaccount-fe/src/store/modules/shop/constants.ts
@@ -3,6 +3,8 @@ export const GET_ALL_SHOPS = 'GET_ALL_SHOPS';
 
 // mutations
 export const MODIFY_ALL_SHOPS = 'MODIFY_ALL_SHOPS';
+export const ADD_NEW_SHOP = 'ADD_NEW_SHOP';
 
-// mutations
+// action 
 export const LOAD_ALL_SHOPS = 'LOAD_ALL_SHOPS';
+
--- SA-fe/shopaccount-fe/src/store/modules/shop/index.ts
+++ SA-fe/shopaccount-fe/src/store/modules/shop/index.ts
@@ -3,11 +3,13 @@ import { State } from './typings';
 import {
   GET_ALL_SHOPS,
   MODIFY_ALL_SHOPS,
-  LOAD_ALL_SHOPS
+  LOAD_ALL_SHOPS,
+  ADD_NEW_SHOP
 } from './constants';
 import { httpRequestSilence } from '@/utils/httpRequest';
 import { IResponse } from '@/typing/vuex/typings';
 import { IShopItem } from '@/typing/shops/typings';
+import { IProductItem } from '@/typing/home/typings';
 
 
 export default {
@@ -30,11 +32,29 @@ export default {
       } catch (error) {
         return Promise.resolve(error);
       }
+    },
+    async [ADD_NEW_SHOP]({ commit }, payload: IShopItem): Promise<string> {
+      try {
+        const { data } = await httpRequestSilence.post<IResponse<IShopItem[]> >(
+          `/shops`, payload);
+        if (data.status || data.msg === 'OK') {
+          commit(MODIFY_ALL_SHOPS, data.data);
+          return Promise.resolve('OK');
+        } else {
+          return Promise.resolve(data.msg);
+        }
+      } catch (error) {
+        return Promise.resolve(error);
+      }
     }
+
   },
   mutations: {
     [MODIFY_ALL_SHOPS](state, payload: IShopItem[]) {
       state.allShops = payload;
+    },
+    [ADD_NEW_SHOP](state, payload: IShopItem) {
+      state.allShops.push(payload);
     }
   },
   getters: {
