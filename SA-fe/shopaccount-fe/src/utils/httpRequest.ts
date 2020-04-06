85657d3719714587a26b0739ceebf47995a1a960
ninanshoulewozaizhe <172685516@qq.com>
2020-04-06 10:45:38 +0800

    调整后台请求接口

--- SA-fe/shopaccount-fe/src/utils/httpRequest.ts
+++ SA-fe/shopaccount-fe/src/utils/httpRequest.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosResponse } from 'axios';
 
 const instanceConfig = {
-  timeout: 5000
+  timeout: 5000,
+  baseURL: '/api'
 };
 
 export const httpRequest = axios.create(instanceConfig); // 请求对象，通常情况下使用此请求器
--- SA-fe/shopaccount-fe/vue.config.js
+++ SA-fe/shopaccount-fe/vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     devServer: {
-      // proxy: {
-      //   '/api': {
-      //     target: 'http://127.0.0.1:5000',
-      //     ws: true,
-      //     changeOrigin: true
-      //   }
-      // }
-      proxy: 'http://127.0.0.1:5000'
+      proxy: {
+        '/api': {
+          target: 'http://129.204.148.253:5000',
+          ws: true,
+          changeOrigin: true
+        }
+      }
+      // proxy: 'http://127.0.0.1:5000'
     }
 }
